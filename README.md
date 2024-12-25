7a95c36918a0260c3587e727be4f467cdc83a928
ssje <2114714800@qq.com>
2024-12-26 00:07:56 +0800

    多态的内容单独存放

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # node-nest
-自己的node后端服务-nest版
+
+自己的node后端服务-nest版,
+作为学习nestjs
---
+++ src/testIdea/class_duotai.ts
@@ -0,0 +1,42 @@
+function login(phone: string) {
+  return `${phone} login success`
+}
+abstract class baseLogin {
+  abstract getUserInfo(): Promise<{ phone: string }>
+  async login(): Promise<void> {
+    const userInfo = await this.getUserInfo()
+    const token = login(userInfo.phone)
+    this.afterLogin(token)
+  }
+  private afterLogin(token: string) {
+    console.log(`登录成功 token: ${token} 保存本地`)
+  }
+}
+
+class DingTalkLogin extends baseLogin {
+  override async getUserInfo() {
+    //获取钉钉的用户信息
+    await new Promise(resolve => setTimeout(resolve, 1000))
+    console.log('获取钉钉的用户信息...')
+    return { phone: '123456789' }
+  }
+}
+
+//企微登录
+class WeChatLogin extends baseLogin {
+  override async getUserInfo() {
+    //获取企微的用户信息
+    await new Promise(resolve => setTimeout(resolve, 1000))
+    console.log('获取企微的用户信息...')
+    return { phone: '987654321' }
+  }
+}
+
+const loginMap = {
+  dingTalk: DingTalkLogin,
+  weChat: WeChatLogin,
+}
+function loginFactory(type: keyof typeof loginMap) {
+  return new loginMap[type]()
+}
+loginFactory('dingTalk').login()
