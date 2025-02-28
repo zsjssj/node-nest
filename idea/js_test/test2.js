8736a8ad943f3e44ac4fcd7c987a34fd7e0c512b
zsj <2114714800@qq.com>
2025-02-28 20:13:21 +0800

    小修改

--- idea/js_test/test2.js
+++ idea/js_test/test2.js
@@ -1,3 +1,4 @@
+const { log } = require('console')
 const { object } = require('joi')
 
 //1.this指向
@@ -67,7 +68,7 @@ const { object } = require('joi')
 }
 
 //5.defineProperty
-;(() => {
+;() => {
   //5.1-es5
   function Product(name, value, num) {
     this.name = name
@@ -108,7 +109,7 @@ const { object } = require('joi')
   const a3 = new Product2('a3', 30, 2)
   a3.num = 5
   console.log('a3.totalValue', a3.totalValue)
-})()
+}
 
 //6.reduce
 ;() => {
@@ -124,5 +125,44 @@ const { object } = require('joi')
   console.log('reduce', res1, res2)
 }
 
-console.log(Object.prototype.toString.call({}), Object.prototype.toString.call([]))
-console.log(Array.isArray({}), Array.isArray([]))
+//7.数组,对象类型判断
+;() => {
+  console.log(Object.prototype.toString.call({}), Object.prototype.toString.call([]))
+  console.log(Array.isArray({}), Array.isArray([]))
+}
+
+//柯里化函数
+;() => {
+  function curry(...args) {
+    let parrms = args
+    const addFn = (...args2) => {
+      parrms = parrms.concat(args2)
+      return addFn
+    }
+    addFn.valueof = () => {
+      return parrms.reduce((pre, cur) => pre + cur)
+    }
+    return addFn
+  }
+
+  const curryAdd = curry(1, 2, 3)(4)(5)(6).valueof()
+  console.log(curryAdd)
+}
+
+//once函数
+;(() => {
+  function once(fn) {
+    let done = false
+    return (...args) => {
+      if (!done) {
+        done = true
+        return fn(...args)
+      }
+    }
+  }
+  const handleOnce = once(a => {
+    console.log('once', a)
+  })
+  handleOnce(12)
+  handleOnce(3)
+})()
